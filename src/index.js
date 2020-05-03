b9c7b234fcf133da1407857854a59f871285fc9d
Chetan Naik <chetan.naik.t@gmail.com>
2020-05-03 16:46:46 +0530

    added parameter validation for GET, PATCH  and DELETE operations

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config();
+
 //Body parser middeware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended:false}));
+
 //queryData route middleware
-app.use('/api/query', require('./routes/api/queryData'));
+app.use('/api/query',require('./routes/api/queryData'));
+
 //postData route middleware
-app.use('/api/add', require('./routes/api/postData'));
+app.use('/api/add',require('./routes/api/postData'));
+
 //updateData route middleware
-app.use('/api/update', require('./routes/api/patchData'));
+app.use('/api/update',require('./routes/api/patchData'));
+
 //deleteData route middleware
-app.use('/api/delete', require('./routes/api/deleteData'));
+app.use('/api/delete',require('./routes/api/deleteData'));
+
 //Connect to database
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, function () { console.log('Connected to db!'); });
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true },
+  ()=>{console.log('Connected to db!')});
+  
 //Set port and listen
-var PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT);
--- src/models/States.js
+++ src/models/States.js
@@ -1,11 +1,13 @@
-var mongooseRest = require('mongoose');
+const mongooseRest = require('mongoose');
+
 //Define the schema
-var statesSchema = mongooseRest.Schema({
-    area: { type: Number, required: true },
-    name: { type: String, required: true },
-    region: { type: String, required: true },
-    state_abbr: { type: String, required: true },
-    population: { type: Number, required: false }
+const statesSchema = mongooseRest.Schema({
+  area:{type:Number,required:true},
+  name:{type:String,required:true},
+  region:{type:String,required:true},
+  state_abbr:{type:String,required:true},
+  population:{type:Number,required:false}
 });
+
 //Export schema
-module.exports = mongooseRest.model('USA_states', statesSchema, 'USA_states');
+module.exports = mongooseRest.model('USA_states',statesSchema,'USA_states');
